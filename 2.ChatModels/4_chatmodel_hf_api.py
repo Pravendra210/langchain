0101d45fc97dbeea8f61335091f84d273db66eb6
parvendrakhenwar210@gmail.com <pravendrakhenwar210@gmail.com>
2026-08-19 21:09:12 +0530

    Initial commit

---
+++ 2.ChatModels/4_chatmodel_hf_api.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HF_TOKEN")
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India?")
+
+print(result.content)
